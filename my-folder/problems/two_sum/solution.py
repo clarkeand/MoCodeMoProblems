7612fb8942148177c0452c7d620fc5ed223caf2f
clarkeand <superclarke909@gmail.com>
2023-06-19 16:27:48 +0000

    Sync LeetCode submission - Two Sum (python)

---
+++ my-folder/problems/two_sum/solution.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_dict = {}
+        
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in num_dict:
+                return [num_dict[diff], i]
+            num_dict[num] = i
+        
+        # if no pair adds up to the target
+        return []
